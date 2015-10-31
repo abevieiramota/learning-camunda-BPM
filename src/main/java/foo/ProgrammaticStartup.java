0071b49168d38136cf9d481a08ef514c2cbedcbb
Abelardo Vieira Mota <abevieiramota@gmail.com>
2015-10-31 19:46:01 -0200

    standalone hello world

---
+++ src/main/java/foo/ProgrammaticStartup.java
@@ -0,0 +1,18 @@
+package foo;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
+
+public class ProgrammaticStartup {
+
+	public static void main(String[] args) {
+		
+		ProcessEngine processEngine = new StandaloneInMemProcessEngineConfiguration().buildProcessEngine();
+		
+		processEngine.getRepositoryService().createDeployment().addClasspathResource("process.bpmn").deploy();
+		
+		processEngine.getRuntimeService().startProcessInstanceByKey("learning-camunda-BPM");
+
+	}
+
+}
