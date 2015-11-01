95f91ff1dda7d0e57c1d91afd599616671a7384a
Abelardo Vieira Mota <abevieiramota@gmail.com>
2015-11-01 10:15:55 -0200

    gateway

---
+++ src/main/java/foo/CreateTwitterDelegate.java
@@ -0,0 +1,26 @@
+package foo;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class CreateTwitterDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		String content = (String) execution.getVariable("content");
+		
+	    AccessToken accessToken = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo", "B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
+	    Twitter twitter = new TwitterFactory().getInstance();
+	    twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
+	    twitter.setOAuthAccessToken(accessToken);
+	 
+	    twitter.updateStatus(content);
+		
+	}
+
+}
--- src/test/java/foo/InMemoryH2Test.java
+++ src/test/java/foo/InMemoryH2Test.java
@@ -3,7 +3,12 @@ package foo;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.init;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
 
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.runtime.ProcessInstantiationBuilder;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.junit.Before;
@@ -15,32 +20,42 @@ import org.junit.Test;
  */
 public class InMemoryH2Test {
 
-  @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+	@Rule
+	public ProcessEngineRule rule = new ProcessEngineRule();
 
-  private static final String PROCESS_DEFINITION_KEY = "learning-camunda-BPM";
+	private static final String PROCESS_DEFINITION_KEY = "learning-camunda-BPM";
 
-  // enable more detailed logging
-  static {
-//    LogUtil.readJavaUtilLoggingConfigFromClasspath(); // process engine
-//    LogFactory.useJdkLogging(); // MyBatis
-  }
+	// enable more detailed logging
+	static {
+		// LogUtil.readJavaUtilLoggingConfigFromClasspath(); // process engine
+		// LogFactory.useJdkLogging(); // MyBatis
+	}
 
-  @Before
-  public void setup() {
-    init(rule.getProcessEngine());
-  }
+	@Before
+	public void setup() {
+		init(rule.getProcessEngine());
+	}
 
-  /**
-   * Just tests if the process definition is deployable.
-   */
-  @Test
-  @Deployment(resources = "process.bpmn")
-  public void testParsingAndDeployment() {
-	  
-	  ProcessInstance processInstance = rule.getRuntimeService().startProcessInstanceByKey("learning-camunda-BPM");
-	  
-	  assertThat(processInstance).isEnded();
-  }
+	/**
+	 * Just tests if the process definition is deployable.
+	 */
+	@Test
+	@Deployment(resources = "process.bpmn")
+	public void testParsingAndDeployment() {
+
+		HashMap<String, Object> processVariables = new HashMap<String, Object>();
+		processVariables.put(
+				"content",
+				"camunda training - XOR gateway brhuehuehue em: "
+						+ new SimpleDateFormat("HH:mm:ss")
+								.format(new GregorianCalendar().getTime()));
+		processVariables.put("xoraBola", Boolean.TRUE);
+
+		ProcessInstance processInstance = rule.getRuntimeService()
+				.startProcessInstanceByKey("learning-camunda-BPM",
+						processVariables);
+
+		assertThat(processInstance).isEnded();
+	}
 
 }
