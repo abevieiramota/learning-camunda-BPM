a7cde545d9083dcb92614a20ef76b1986f10eaba
Abelardo Vieira Mota <abevieiramota@gmail.com>
2015-11-01 12:00:04 -0200

    hands on completed

--- src/main/java/foo/CreateTwitterDelegate.java
+++ src/main/java/foo/CreateTwitterDelegate.java
@@ -11,16 +11,23 @@ public class CreateTwitterDelegate implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		
+
 		String content = (String) execution.getVariable("content");
+
+		String nome = (String) execution.getVariable("nome");
 		
-	    AccessToken accessToken = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo", "B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
-	    Twitter twitter = new TwitterFactory().getInstance();
-	    twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
-	    twitter.setOAuthAccessToken(accessToken);
-	 
-	    twitter.updateStatus(content);
-		
+		content = content + " por " + nome;
+
+		AccessToken accessToken = new AccessToken(
+				"220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo",
+				"B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
+		Twitter twitter = new TwitterFactory().getInstance();
+		twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk",
+				"gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
+		twitter.setOAuthAccessToken(accessToken);
+
+		twitter.updateStatus(content);
+
 	}
 
 }
